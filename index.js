55105fcff4d290a772b446ce94ef2a89e3bcb39d
Gonzalo Valenzuela <gvalenzuelac@live.com>
2024-02-28 02:58:31 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+precio = 400000;
+cantidad = 0;
+valorTotal = 0;
+
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio;
+
+valorSpan = document.querySelector(".valor-total");
+valorSpan.innerHTML = valorTotal;
+
+
+
+
